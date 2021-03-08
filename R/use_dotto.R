e4871548491e808b32366f9bc5466beaf82c3be7
ardeeshany <ardeeshany@gmail.com>
2021-03-07 20:26:53 -0500

    Add different engines

--- R/use_dotto.R
+++ R/use_dotto.R
@@ -17,21 +17,34 @@
 #' DataMotto::use_dotto()
 #'}
 use_dotto <- function() {
-  knitr::opts_chunk$set(Dot_active = FALSE)
+  default_engines <- c("R", "python", "julia", "sql", "node", "Rcpp", "d3")
+  knitr::opts_chunk$set(Dot_title = NULL,
+                        Dot_active = FALSE,
+                        Dot_color = NULL,
+                        Dot_open = T,
+                        Dot_close = T)
   knitr::knit_hooks$set(Dot_title = function(before, options){
     if(before){
+      if(options$Dot_open){
+      if(is.null(options$Dot_color)){
+      dot_col <- ifelse(options$engine %in% default_engines, options$engine, "others")
+      } else {
+      dot_col <- ifelse(options$Dot_color %in% default_engines, options$Dot_color, "others")
+      }
+
       paste(
         '<li class="nav-item dot-li">',
-        glue::glue('<a class="dot-li nav-link {ifelse(options$Dot_active, "active", "")} dm-dot-title"> {options$Dot_title} </a>'),
+        glue::glue('<a class="dot-li nav-link {ifelse(options$Dot_active, "active", "")} {paste0("dot-li-",{dot_col})} dm-dot-title"> {options$Dot_title} </a>'),
         glue::glue('<div class="dm-dot-content {ifelse(options$Dot_active, "active", "")}">'),
         sep = "\n"
-      )
+      ) }
     } else {
+      if(options$Dot_close){
       paste(
         '</div>',
         '</li>',
         sep = "\n"
-      )
+      )}
     }
   })
 }
