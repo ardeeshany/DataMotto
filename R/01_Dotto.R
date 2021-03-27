f96606e7c8fd5773b167da3ce4625bc19f8bc6b7
ardeeshany <ardeeshany@gmail.com>
2021-03-27 12:55:46 -0400

    Update twitter protocol

--- R/01_Dotto.R
+++ R/01_Dotto.R
@@ -32,7 +32,8 @@ Dotto <- function(fig_width = 6,
                        pandoc_args = NULL,
                        md_extensions = "-autolink_bare_uris",
                        self_contained = FALSE,
-                       in_header = c(open_graph_dotto()),
+                       in_header = c(social_card_protocol_dotto()$open_graph,
+                                     social_card_protocol_dotto()$twitter),
                        before_body = here::here("resources/header_post.html"),
                        after_body = c(discus_dotto(),
                                       here::here("resources/footer.html")),
@@ -74,30 +75,47 @@ Dotto_dependency <- function() {
 }
 
 # Open Graph Protocol
-open_graph_dotto <- function() {
+social_card_protocol_dotto <- function() {
   dir_name <- basename(getwd())
   base_url <- rmarkdown::site_config(here::here())$base_url
   rmd_path <- list.files(getwd(), full.names = T, pattern = "\\.Rmd$")
   all_yml_metadata <- rmarkdown::yaml_front_matter(rmd_path)
   # ---
-  canonical_link = paste0(base_url, glue::glue("/posts/Dotto/{dir_name}/index.html"))
+  canonical_link = paste0(base_url, glue::glue("/posts/Dotto/{dir_name}/"))
   # ---
   cover_image_url <- resolve_cover_image(rmd_path) %>% stringr::str_remove("^\\.")
   canonical_img_url <- paste0(base_url, cover_image_url)
 
+  # --- Open Graph Card
   og_codes <- sprintf('
   <meta property="og:title" content="%s" />
   <meta property="og:type" content="article">
   <meta property="og:description" content="%s" />
   <meta property="og:url" content="%s" />
+  <meta property="og:locale" content="en_US">
+  <meta property="og:site_name" content="Data Motto">
   <meta property="og:image" content="%s" />
   ', all_yml_metadata$title, all_yml_metadata$description, canonical_link, canonical_img_url)
   # ---
-  temp_file <- tempfile()
-  con <- file(temp_file, open = "w", encoding = "UTF-8")
+  og_protocol <- tempfile()
+  con <- file(og_protocol, open = "w", encoding = "UTF-8")
   xfun::write_utf8(og_codes, con)
   close(con)
-  return(temp_file)
+  # --- Twitter Card
+  twitter_codes <- sprintf('
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:title" content="%s">
+  <meta property="twitter:description" content="%s">
+  <meta property="twitter:url" content="%s">
+  <meta property="twitter:image" content="%s">
+  ', all_yml_metadata$title, all_yml_metadata$description, canonical_link, canonical_img_url)
+  # ---
+  twitter_protocol <- tempfile()
+  con <- file(twitter_protocol, open = "w", encoding = "UTF-8")
+  xfun::write_utf8(twitter_codes, con)
+  close(con)
+
+  return(list(open_graph = og_protocol, twitter = twitter_protocol))
 
 }
 
