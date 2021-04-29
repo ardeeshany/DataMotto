30fbfd3861c7de11fda1b47e0f2625dea132bc8f
ardeeshany <ardeeshany@gmail.com>
2021-04-29 08:34:51 -0400

    Add Index.html

--- R/01_Index.R
+++ R/01_Index.R
@@ -29,54 +29,65 @@ Index <- function(fig_width = 6,
                        self_contained = F,
                        in_header = NULL,
                        before_body = here::here("./resources/header.html"),
-                       after_body = here::here("./resources/footer.html"),
+                       after_body = NULL,
+                       #after_body = here::here("./resources/footer.html"),
                        ...) {
-  default_template(
-    template_name = "Index_page",
-    template_path = "templates/Index_page.html",
-    template_dependencies = list(
-      Index_page_dependency()
-    ),
-    pandoc_args = pandoc_args,
-    fig_width = fig_width,
-    fig_height = fig_height,
-    fig_caption = fig_caption,
+
+  html_document(#template = system.file("templates/Dotto.html", package = "DataMotto"),
+    template = here::here("inst/templates/Index_page.html"),
+    includes = list(in_header = in_header,
+                    before_body = before_body,
+                    after_body = after_body),
     highlight = highlight,
-    lightbox = lightbox,
-    thumbnails = thumbnails,
-    md_extensions = md_extensions,
-    self_contained = self_contained,
-    in_header = in_header,
-    before_body = before_body,
-    after_body = after_body,
-    # mathjax = mathjax,
-    ...
-  )
+    mathjax = mathjax,
+    self_contained = self_contained)
+
+  # default_template(
+  #   template_name = "Index_page",
+  #   template_path = "templates/Index_page.html",
+  #   template_dependencies = list(
+  #     Index_page_dependency()
+  #   ),
+  #   pandoc_args = pandoc_args,
+  #   fig_width = fig_width,
+  #   fig_height = fig_height,
+  #   fig_caption = fig_caption,
+  #   highlight = highlight,
+  #   lightbox = lightbox,
+  #   thumbnails = thumbnails,
+  #   md_extensions = md_extensions,
+  #   self_contained = self_contained,
+  #   in_header = in_header,
+  #   before_body = before_body,
+  #   after_body = after_body,
+  #   # mathjax = mathjax,
+  #   ...
+  # )
 }
 
 
 # Dependency added manually (not from a library)
-Index_page_dependency <- function() {
-  htmltools::htmlDependency(name = "Index_page",
-                            version = "0.1.0",
-                            src = system.file("templates/DataMotto", package = "DataMotto"),
-                            head = list(paste0(
-                              # "<script> var likebtn =",
-                              # jsonlite::toJSON(get_likebtn(),
-                              #                  auto_unbox = F,
-                              #                  pretty = T),
-                              # "</script>",
-                              "<script> var json =",
-                              jsonlite::toJSON(config_posts(),
-                                               auto_unbox = F,
-                                               pretty = T),
-                              "</script>",
-                              '<link rel="shortcut icon" href="',rmarkdown::site_config(input = here::here())$favicon,'">'
-                              )),
-                            script = "Index_page.js",
-                            stylesheet = "site.css",
-                            all_files = F)
-}
+# Index_page_dependency <- function() {
+#   htmltools::htmlDependency(name = "Index_page",
+#                             version = "0.1.0",
+#                             src = system.file("templates/DataMotto", package = "DataMotto"),
+#                             head = list(paste0(
+#                               # "<script> var likebtn =",
+#                               # jsonlite::toJSON(get_likebtn(),
+#                               #                  auto_unbox = F,
+#                               #                  pretty = T),
+#                               # "</script>",
+#                               "<script> var json =",
+#                               jsonlite::toJSON(config_posts(),
+#                                                auto_unbox = F,
+#                                                pretty = T),
+#                               "</script>",
+#                               '<link rel="shortcut icon" href="',rmarkdown::site_config(input = here::here())$favicon,'">'
+#                               )),
+#                             script = "Index_page.js",
+#                             stylesheet = "site.css",
+#                             all_files = F)
+# }
 
 
 
---
+++ docs/assets/js/carts.js
@@ -0,0 +1,11 @@
+import Vue from './vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  },
+  methods: {
+    
+  }
+})
