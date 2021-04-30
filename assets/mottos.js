282d0f985de8fed51ce93025842cffd78060fefd
ardeeshany <ardeeshany@gmail.com>
2021-04-30 06:23:13 -0400

    update index page

---
+++ assets/mottos.js
@@ -0,0 +1,125 @@
+export default {
+  "D001-2021-04-04-analyzing-palmer-penguins-R": {
+    file_name: ["analyzing-palmer-penguins-R"],
+    dir_name: ["D001-2021-04-04-analyzing-palmer-penguins-R"],
+    cover_image_url: [
+      "https://allisonhorst.github.io/palmerpenguins/reference/figures/logo.png",
+    ],
+    link: [
+      "posts/Dotto/D001-2021-04-04-analyzing-palmer-penguins-R/index.html",
+    ],
+    base_url: ["https://datamotto.com"],
+    social_networks_datamotto: {
+      twitter: ["https://twitter.com/DataMotto"],
+      linkedin: ["https://www.linkedin.com/company/datamotto"],
+    },
+    dotto_number: [1],
+    dotto_label: ["D001"],
+    dotto_id: ["af89baec-954c-11eb-babc-acbc32baed85"],
+    family_id: ["af89be66-954c-11eb-babc-acbc32baed85"],
+    dotto_label_base_in_family: ["D001"],
+    dotto_id_base_in_family: ["af89be66-954c-11eb-babc-acbc32baed85"],
+    line_ids: ["af89be66-954c-11eb-babc-acbc32baed85"],
+    title: ["Analyzing Palmer Penguins"],
+    description: [
+      "The data for 344 penguins. There are 3 different species of penguins in this dataset, collected from 3 islands in the Palmer Archipelago, Antarctica.\n",
+    ],
+    author: [
+      {
+        name: ["Ardalan Mirshani"],
+        occupation: ["Sr Expert Data Scientist"],
+        affiliation: ["Novartis"],
+        url: ["mirshani.com"],
+        img: ["ArdalanMirshani.jpeg"],
+        lang: ["r"],
+        profile_img: [
+          "./analyzing-palmer-penguins-R_files/figure-html/ArdalanMirshani.jpeg",
+        ],
+      },
+      {
+        name: ["Amin Zamani"],
+        occupation: ["Software developer"],
+        affiliation: ["DataMotto"],
+        url: ["Aminzamani.com"],
+        img: ["AminZamani.jpeg"],
+        lang: ["python", "julia"],
+        profile_img: [
+          "./analyzing-palmer-penguins-R_files/figure-html/AminZamani.jpeg",
+        ],
+      },
+    ],
+    tech: [
+      {
+        lang: ["r"],
+        pkgs: ["ggplot2", "dplyr"],
+      },
+      {
+        lang: ["python"],
+        pkgs: ["pandas"],
+      },
+    ],
+    date: {
+      created: ["2021-04-04"],
+      last_update: ["2021-04-12"],
+    },
+    categories: [
+      "Importing",
+      "Cleaning",
+      "Manipulation",
+      "Visualization",
+      "Reporting",
+    ],
+    applications: ["Finance", "Pharma"],
+    cover_image: [
+      "https://allisonhorst.github.io/palmerpenguins/reference/figures/logo.png",
+    ],
+    slug: ["analyzing-palmer-penguins"],
+    output: ["DataMotto::Dotto"],
+  },
+  "D002-2021-04-28-coffee-market-in-paris": {
+    file_name: ["coffee-market-in-paris"],
+    dir_name: ["D002-2021-04-28-coffee-market-in-paris"],
+    cover_image_url: ["resources/images/default_dotto.jpg"],
+    link: ["posts/Dotto/D002-2021-04-28-coffee-market-in-paris/index.html"],
+    base_url: ["https://datamotto.com"],
+    social_networks_datamotto: {
+      twitter: ["https://twitter.com/DataMotto"],
+      linkedin: ["https://www.linkedin.com/company/datamotto"],
+    },
+    dotto_number: [2],
+    dotto_label: ["D002"],
+    dotto_id: ["3cec3bca-a81d-11eb-b8a7-acbc32baed85"],
+    title: ["Coffee Market in Paris"],
+    description: ["A short description of the Dotto.\n"],
+    author: [
+      {
+        name: ["Audrey Hepburn"],
+        occupation: ["Actress"],
+        affiliation: ["Warner Brothers"],
+        url: ["https://en.wikipedia.org/wiki/Audrey_Hepburn"],
+        img: ["audrey.jpeg"],
+        lang: ["r", "julia"],
+        profile_img: ["./coffee-market-in-paris_files/figure-html/audrey.jpeg"],
+      },
+    ],
+    tech: [
+      {
+        lang: ["r"],
+        pkgs: ["dplyr"],
+      },
+      {
+        lang: ["julia"],
+        pkgs: {},
+      },
+    ],
+    date: {
+      created: ["2021-04-28"],
+      last_updated: ["2021-04-28"],
+    },
+    categories: ["visualization"],
+    applications: ["cinema"],
+    cover_image: {},
+    slug: ["coffee-market-in-paris"],
+    output: ["DataMotto::Dotto"],
+  },
+};
